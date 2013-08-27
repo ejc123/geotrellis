b4abe7b77e13feff75c8b88d8471a3311706ae74
Josh Marcus <jmarcus@azavea.com>
2013-08-27 17:57:11 -0400

    Add partition/chunk type to DataSource.
    Significant additional refactor, as well.

--- src/test/scala/geotrellis/DataSourceSpec.scala
+++ src/test/scala/geotrellis/DataSourceSpec.scala
@@ -6,7 +6,7 @@ import org.scalatest.matchers.ShouldMatchers
 
 import geotrellis.raster._
 import geotrellis.raster.op._
-import geotrellis.statistics.Histogram
+import geotrellis.statistics._
 
 @org.junit.runner.RunWith(classOf[org.scalatest.junit.JUnitRunner])
 class DataSourceSpec extends FunSpec 
@@ -30,12 +30,64 @@ class DataSourceSpec extends FunSpec
       result2.get(100,100) should be (3236)
       result3.get(100,100) should be (3239)
     }
+
     it ("should return a DistributedSeqSource when appropriate") {
       val d = getDistributedRasterSource
-      val s:DistributedSeqSource[Histogram] = d.histogram
+      // distributed source of histograms
+
+      // DataSource[Histogram,_] -- we can get a single histogram
+      val hist:SingleDataSource[Histogram,Histogram] = d.histogram // distributed
+      
+      case class MinFromHistogram(h:Op[Histogram]) extends Op1(h)({
+        (h) => Result(h.getMinValue)
+      })
+
+      case class FindMin(ints:Op[Seq[Int]]) extends Op1(ints)({
+        (ints) => Result(ints.reduce(math.min(_,_)))
+      })
+
+      val ints:DistributedSeqSource[Int] = hist.map(MinFromHistogram(_))
+
+      val int:SingleDataSource[Int,Int] = ints.converge.map(FindMin(_))
+
+      // ints.withResult( seq => seq.reduce(min(_,_)) )
+      
+      // val result = int.map(FindMin(_))
+
+
+      //ts.converge
+//      val min = ints.get.map(FindMin(_))
+
+      /*
+      s.map(_.min)
+       .reduce(math.min(_,_))
+       .run(server)
+ */
+      // val r = io.LoadRaster("asdf")
+      // val r2 = io.LoadRaster("asdff")
+      // val r3 = io.LoadRaster("asdfff")
+      // val add = local.Add(r,r2,r3)
+
+      // val add = io.LoadRaster.flatMap( r => local.Add(r,5))
+
+      // single histogram
+//      val s2:Op[Histogram] = d.histogram
+      
+      val histogramResult = runSource(hist)
+      val intsResult = runSource(ints)
+      //val minResult = run(min)
+
+  //    val histogramResult = run(s2)
+      val s = histogramResult
       println(s)
+      println(histogramResult)
+      histogramResult.getMinValue should be (2231)
+      histogramResult.getMaxValue should be (8367)
+      intsResult.length should be (12)
+  //    minResult should be (2231)
     }
   }
+   
 
   describe("LocalRasterSource") {
     it("should return a LocalRasterSource when possible") {
